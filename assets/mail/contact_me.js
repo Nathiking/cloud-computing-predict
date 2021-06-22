315b4ce09574f5865033f20758beadb3cb1ab945
Nathiking <83448029+Nathiking@users.noreply.github.com>
2021-06-22 20:39:55 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://2dt8hs15fi.execute-api.eu-west-1.amazonaws.com/default/Explore-Predict",
+                url: "https://omgm2lc6id.execute-api.eu-west-1.amazonaws.com/default/Cloud-Function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
